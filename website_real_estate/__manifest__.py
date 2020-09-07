4b9d0b36b4e284d750526497218661de324c69bc
jonathan <jon.rey8995@gmail.com>
2020-09-07 03:20:12 -0400

    prueba 1

---
+++ website_real_estate/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of AppJetty. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Website sale product lightbox',
+    'description': '',
+    'summary': '',
+    'category': 'Website Sale',
+    'version': '13.0.1.0.0',
+    'author': 'Soluciones Softhard',
+    'license': 'OPL-1',
+    'website': 'www.solucionessofthard.com',
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_template.xml',
+    ],
+    'images': [],
+    'price': 29.00,
+    'currency': 'EUR',
+    'application': True,
+}
---
+++ website_real_estate/static/src/js/real_estate.js
@@ -0,0 +1,9 @@
+odoo.define('website_sale_product_lightbox.website_sale_product_lightbox', function(require) {
+    "use strict";
+    
+    console.log("hola, que tal")
+
+});
+
+
+
